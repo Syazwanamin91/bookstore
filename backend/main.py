d960d0948171f79edc01f5e2b1c454d0c8903d14
syazwanamin91 <m.syazwan.amin@gmail.com>
2025-08-07 21:38:14 +0800

    added cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from db.db_connection import init_table
 from routers.book_router import router as book_router
 
 init_table()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #add your frontend URL here if needed
+    allow_credentials=True,         
+    allow_methods=["*"], #allow all methods
+    allow_headers=["*"], #allow all headers for example: Authorization, Content-Type
+)
 
 @app.get("/")
 def read_root():
